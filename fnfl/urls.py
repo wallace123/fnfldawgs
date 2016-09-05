e2250e338fa109c2db537ec96d9b102226762261
wallace123 <bwtibbs@hotmail.com>
2016-09-05 19:01:00 -0400

    Adding login and logout functionality

--- fnfl/urls.py
+++ fnfl/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     url(r'^lineup/(?P<pk>\d+)/$', views.lineup_detail, name='lineup_detail'),
     url(r'^lineup/new/$', views.lineup_new, name='lineup_new'),
     url(r'^lineup/(?P<pk>\d+)/edit/$', views.lineup_edit, name='lineup_edit'),
+    url(r'^drafts/$', views.lineup_draft_list, name='lineup_draft_list'),
+    url(r'^lineup/(?P<pk>\d+)/publish/$', views.lineup_publish, name='lineup_publish'),
+    url(r'^lineup/(?P<pk>\d+)/remove/$', views.lineup_remove, name='lineup_remove'),
 ]
--- fnfl/views.py
+++ fnfl/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Lineup
 from .forms import LineupForm
+from django.contrib.auth.decorators import login_required
 
 def lineup_list(request):
     lineups = Lineup.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -11,19 +12,20 @@ def lineup_detail(request, pk):
     lineup = get_object_or_404(Lineup, pk=pk)
     return render(request, 'fnfl/lineup_detail.html', {'lineup': lineup})
 
+@login_required
 def lineup_new(request):
     if request.method == "POST":
         form = LineupForm(request.POST)
         if form.is_valid():
             lineup = form.save(commit=False)
             lineup.author = request.user
-            lineup.published_date = timezone.now()
             lineup.save()
             return redirect('lineup_detail', pk=lineup.pk)
     else:
         form = LineupForm()
     return render(request, 'fnfl/lineup_edit.html', {'form': form})
 
+@login_required
 def lineup_edit(request, pk):
     lineup = get_object_or_404(Lineup, pk=pk)
     if request.method == "POST":
@@ -31,9 +33,25 @@ def lineup_edit(request, pk):
         if form.is_valid():
             lineup = form.save(commit=False)
             lineup.author = request.user
-            lineup.published_date = timezone.now()
             lineup.save()
             return redirect('lineup_detail', pk=lineup.pk)
     else:
         form = LineupForm(instance=lineup)
     return render(request, 'fnfl/lineup_edit.html', {'form': form})
+
+@login_required
+def lineup_draft_list(request):
+    lineups = Lineup.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'fnfl/lineup_draft_list.html', {'lineups': lineups})
+
+@login_required
+def lineup_publish(request, pk):
+    lineup = get_object_or_404(Lineup, pk=pk)
+    lineup.publish()
+    return redirect('fnfl.views.lineup_detail', pk=pk)
+
+@login_required
+def lineup_remove(request, pk):
+    lineup = get_object_or_404(Lineup, pk=pk)
+    lineup.delete()
+    return redirect('fnfl.views.lineup_list')
