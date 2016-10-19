ef4f8f223c3c31f1192a33a04b885cb1670d9d2e
wallace123 <bwtibbs@hotmail.com>
2016-10-19 18:50:51 -0400

    update settings.py for database

--- mysite/settings.py
+++ mysite/settings.py
@@ -136,6 +136,7 @@ if LOCAL_DB:
     }
 else:
     import dj_database_url
+    DATABASES = {}
     DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
